5f1d8ea1dbba37ebc552bd9289d7a1cbe1a0b7a3
melgazar9 <melgazar9@gmail.com>
2022-08-23 18:38:32 -0500

    adding reqs and seteup

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages 
+
+setup(name='ds_core',
+      version='0.1',
+      packages=find_packages(),
+      author='Matt Elgazar',
+      include_package_data=True)
+
