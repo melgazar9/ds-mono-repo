410cdc379f677f6559587a2c335dee1e422a994e
Matt Elgazar <melgazar9@gmail.com>
2022-11-22 13:53:21 -0600

    cronjob to be ran in production

---
+++ projects/financial_elt/yfinance/cronjob.sh
@@ -0,0 +1,14 @@
+0 */6 * * * cd ~/ds-mono-repo/projects/financial_elt/yfinance/; git pull; source venv/bin/activate; pip install -r requirements.txt; python populate_database.py > logs/populate_database_$(date +%s).log 2&>1
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+                                                                                             25,5          All
