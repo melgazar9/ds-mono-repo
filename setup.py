2fe539a66b180ae41b33569554e7c68e93921248
Matt Elgazar <melgazar9@gmail.com>
2023-03-28 00:25:09 -0500

    rm

--- setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup, find_packages 
-
-setup(name='ds-core',
-      version='0.1',
-      packages=find_packages(),
-      author='Matt Elgazar',
-      include_package_data=True)
