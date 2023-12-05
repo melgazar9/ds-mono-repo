32814fb43092bd3ba3c8f95f99c8b03447fb0a58
melgazar9 <melgazar9@gmail.com>
2023-12-05 12:39:43 -0600

    edit run for gcp cloud logging

--- projects/financial_elt/yfinance/tap-yfinance/run.sh
+++ projects/financial_elt/yfinance/tap-yfinance/run.sh
@@ -1,3 +1,10 @@
 set -e
 
-sudo docker-compose up --build > logs/docker_logs_$(date +%s).log 2>&1
+# log to a local file
+# sudo docker-compose up --build > logs/docker_logs_$(date +%s).log 2>&1
+
+# log to syslog
+# sudo docker-compose up --build | logger 2>&1
+
+# gcp cloud logging
+sudo docker-compose up --build | tee >(while IFS= read -r line; do gcloud logging write tap_yfinance_log --severity=INFO --message="$line"; done)
