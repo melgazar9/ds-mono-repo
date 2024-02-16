fd94edd410a68c9ad06f44d01fac3870e49d20e0
OEM Configuration (temporary user) <melgazar9@gmail.com>
2024-02-16 01:57:18 -0600

    bugfix gcp logging

--- projects/financial_elt/tap-yfinance/gcp_logging.py
+++ projects/financial_elt/tap-yfinance/gcp_logging.py
@@ -1,13 +1,13 @@
 import sys
-import google.cloud.logging
-from google.cloud.logging.handlers import CloudLoggingHandler
 
 def get_handler(*, project=None, logger_name=None):
     """Return a handler based on the environment."""
     try:
+        import google.cloud.logging
+        from google.cloud.logging.handlers import CloudLoggingHandler
+
         client = google.cloud.logging.Client(project=project)
         return CloudLoggingHandler(client, name=logger_name)
-
     except Exception as e:
         print(f"Failed to create logging client: {e}", file=sys.stderr)
         raise
