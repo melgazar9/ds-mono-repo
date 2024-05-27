cc1be0e5f665d1c8735de8a3e2392d9355ee3eb9
matt <melgazar9@gmail.com>
2024-05-26 22:55:15 -0500

    update gcp_logging py file

--- projects/financial_elt/tap-yfinance/gcp_logging.py
+++ projects/financial_elt/tap-yfinance/gcp_logging.py
@@ -1,5 +1,6 @@
 import sys
 
+import logging
 
 def get_handler(*, project=None, logger_name=None):
     """Return a handler based on the environment."""
@@ -8,7 +9,10 @@ def get_handler(*, project=None, logger_name=None):
         from google.cloud.logging.handlers import CloudLoggingHandler
 
         client = google.cloud.logging.Client(project=project)
-        return CloudLoggingHandler(client, name=logger_name)
+        handler = CloudLoggingHandler(client, name=logger_name, batch_size=32000, batch_timeout=5.0)
+
+        handler.setFormatter(logging.Formatter('%(message)s'))
+        return handler
     except Exception as e:
         print(f"Failed to create logging client: {e}", file=sys.stderr)
         raise
