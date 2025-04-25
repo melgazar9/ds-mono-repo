f883dfea2d8d3b92f90dad89bcb15826d3bc7791
matt <melgazar9@gmail.com>
2025-04-25 13:36:29 -0500

    fix filestream logging within kibana

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-sudo docker-compose build
-
-sudo docker-compose up -d
+sudo chown root:root ./filebeat/filebeat.yml && \
+sudo docker compose build && \
+sudo docker compose up -d
 
 # get the url with code to access kibana
 sudo docker compose logs kibana | grep Go
