cf6fb0e5b02cf42b43a0592e33e5b429a4d02467
melgazar9 <melgazar9@gmail.com>
2025-04-27 11:36:36 -0500

    bugfix docker build and logging cleanup

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
-sudo chown root:root ./filebeat/filebeat.yml && \
+set -e
+
+#sudo chown root:root ./filebeat/filebeat.yml && \ ---> if not root it might need to be set to root privaleges on linux
+
 sudo docker compose build && \
 sudo docker compose up -d
 
