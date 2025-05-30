6697de140c6bb4495d9eb8463501c2c6c4287fd1
melgazar9 <melgazar9@gmail.com>
2025-05-30 01:17:40 -0500

    log return code for each completed subprocess

--- projects/financial_elt/utils.py
+++ projects/financial_elt/utils.py
@@ -151,9 +151,8 @@ def run_process_task(run_commands, cwd, concurrency_semaphore):
     try:
         for _ in range(len(run_commands)):
             try:
-                command, return_code = return_queue.get(
-                    timeout=60
-                )  # 60-second timeout for getting from queue
+                command, return_code = return_queue.get(timeout=60)  # 60-second timeout for getting from queue
+                logging.info(f"SUBPROCESS COMMAND: {command} FINISHED WITH RETURN CODE ---> return_code: {return_code}")
                 cmd_return_codes[str(command)] = return_code
             except Empty:
                 logging.error(
