0044ab4af1e91967229ab207a7779dbede344a4d
melgazar9 <melgazar9@gmail.com>
2025-09-25 15:46:09 +0700

    fix setup_filebeat.sh

--- setup_filebeat.sh
+++ setup_filebeat.sh
@@ -2,4 +2,4 @@ ENVIRONMENT=${1:-dev}
 
 sudo chown root:root ./filebeat/filebeat.yml
 sudo chmod go-w ./filebeat/filebeat.yml
-sudo docker compose -f docker-compose.$ENVIRONMENT.yml restart filebeat
+sudo docker compose -f docker-compose-$ENVIRONMENT.yml restart filebeat
